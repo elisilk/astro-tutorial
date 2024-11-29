458ed9c4977a7bd0e13b7795820e1adcf87e7613
Eli Silk <esilk@me.com>
2024-11-29 09:02:48 -0500

    Added RSS feed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://legendary-gingersnap-39a087.netlify.app/",
+});
